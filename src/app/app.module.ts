29b7b4f556c4b297cb537fdd77b2e64f5071050c
Abid Waqar <abidwaqar98@gmail.com>
2022-08-30 22:41:06 +0500

    add abount page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { ButtonComponent } from './components/button/button.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
     declarations: [
@@ -19,8 +22,24 @@ import { TasksComponent } from './components/tasks/tasks.component';
         TasksComponent,
         TaskItemComponent,
         AddTaskComponent,
+        AboutComponent,
+        FooterComponent,
     ],
-    imports: [BrowserModule, FontAwesomeModule, HttpClientModule, FormsModule],
+    imports: [
+        BrowserModule,
+        FontAwesomeModule,
+        HttpClientModule,
+        FormsModule,
+        RouterModule.forRoot(
+            [
+                { path: '', component: TasksComponent },
+                { path: 'about', component: AboutComponent },
+            ],
+            {
+                enableTracing: true,
+            }
+        ),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
